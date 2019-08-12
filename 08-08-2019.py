d3d718f1c89d0e80bf4ab80b4815023f43fbfa9f
Jonathan Dorety <kyullog@gmail.com>
2019-08-12 08:06:55 -0700

    First day done

---
+++ 08-08-2019.py
@@ -0,0 +1,11 @@
+def print_three_numbers(first_num, second_num, third_num):
+    print(f"first num is : {first_num}")
+    print(f"second num is : {second_num}")
+    print(f"third num is : {third_num}")
+
+
+print_three_numbers(1, 4, 2)
+
+print_three_numbers(third_num=2, second_num=4, first_num=1)
+
+print_three_numbers(1, third_num=2, second_num=4)
